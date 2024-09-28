e438a18ea87e10c3e04c908b5d46e7f14ca41186
jairock007 <jairock7776@gmail.com>
2024-09-28 20:30:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/header/header";
-import Nav from "./components/nav/Nav.jsx";
+import Nav from "./components/nav/Nav";
 import About from "./components/about/about";
 import Experience from "./components/experience/experience";
 import Services from "./components/services/services";
