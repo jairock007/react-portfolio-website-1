de7fdbee407a91d4dd814a244e64753a907202be
jairock007 <jairock7776@gmail.com>
2024-09-28 20:37:13 +0530

    Update App.jsx

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,58 +1,54 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/me-about.jpg'
-import { FaAward } from 'react-icons/fa'
-import { FiUsers } from 'react-icons/fi'
-import { VscFolderActive } from 'react-icons/vsc'
-
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderActive } from "react-icons/vsc";
 
 const about = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get to know</h5>
       <h2>About Me</h2>
-      <div className='container about_container'>
-        <div className='about_me'>
-          <div className='about_me-image'>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
             <img src={ME} alt="jitendra" />
           </div>
         </div>
 
-        <div className='about_content'>
-          <div className='about_cards'>
-            <article className='about_card'>
-              <FaAward className='about-card' />
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about-card" />
               <h5>Experience</h5>
               <small>1 Year Working</small>
             </article>
-            <article className='about_card'>
-              <FiUsers className='about-card' />
+            <article className="about_card">
+              <FiUsers className="about-card" />
               <h5>Clients</h5>
               <small>10+ India</small>
             </article>
-            <article className='about_card'>
-              <VscFolderActive className='about-card' />
+            <article className="about_card">
+              <VscFolderActive className="about-card" />
               <h5>Projects</h5>
               <small>14+ Completed</small>
             </article>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit provident, ab ipsum asperiores excepturi sequi tempora consequuntur accusamus veniam distinctio, doloremque quasi libero, eveniet ducimus qui nobis iusto sint voluptatem?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+            provident, ab ipsum asperiores excepturi sequi tempora consequuntur
+            accusamus veniam distinctio, doloremque quasi libero, eveniet
+            ducimus qui nobis iusto sint voluptatem?
           </p>
 
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
+  );
+};
 
-
-
-
-
-
-
-
-  )
-}
-
-export default about
+export default about;
