8b39f1c64094d0ce2088f3cadb14eb28add659f4
jairock007 <jairock7776@gmail.com>
2024-09-28 13:28:41 +0530

    Update testimonials.jsx

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 
 const portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="portfolio" />
+          </div>
+          <h3>Crypto currency Dashboard & Financial Visualization </h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG2} alt="portfolio" />
+          </div>
+          <h3>Charts Templates & infographic in Figma</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG3} alt="portfolio" />
+          </div>
+          <h3>Figma Dashboard UI Design</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG4} alt="portfolio" />
+          </div>
+          <h3>Maintaining tasks and tracking progress</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG5} alt="portfolio" />
+          </div>
+          <h3>Charts Templates & infographic in Figma</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG6} alt="portfolio" />
+          </div>
+          <h3>Charts Templates & infographic in Figma</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,10 +1,76 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVATAR1 from "../../assets/avatar1.jpg";
+import AVATAR2 from "../../assets/avatar2.jpg";
+import AVATAR3 from "../../assets/avatar3.jpg";
+import AVATAR4 from "../../assets/avatar4.jpg";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const testimonials = () => {
   return (
-    <section id='testimonials'>testimonials</section>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-export default testimonials
+      <Swiper
+        className="container testimonials_container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide className="testimonial">
+          <div className="client_avatar">
+            <img src={AVATAR1} alt="avatar1" />
+          </div>
+          <h5 className="client_name">Jerry Achiever</h5>
+          <small className="client_review">
+            I get a good impression, I carry out my project with all the
+            possible quality and attention and support 24 hours a day
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client_avatar">
+            <img src={AVATAR2} alt="avatar1" />
+          </div>
+          <h5 className="client_name">Tom Achiever</h5>
+          <small className="client_review">
+            I get a good impression, I carry out my project with all the
+            possible quality and attention and support 24 hours a day
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client_avatar">
+            <img src={AVATAR3} alt="avatar1" />
+          </div>
+          <h5 className="client_name">Ernest Achiever </h5>
+          <small className="client_review">
+            I get a good impression, I carry out my project with all the
+            possible quality and attention and support 24 hours a day
+          </small>
+        </SwiperSlide>
+        <SwiperSlide className="testimonial">
+          <div className="client_avatar">
+            <img src={AVATAR4} alt="avatar1" />
+          </div>
+          <h5 className="client_name">Logan Achiever</h5>
+          <small className="client_review">
+            I get a good impression, I carry out my project with all the
+            possible quality and attention and support 24 hours a day
+          </small>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
+};
+
+export default testimonials;
